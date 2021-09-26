07cedb467535d2a045da2378639006f40158920f
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-09-26 02:48:11 -0300

    add MasksName type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@ export * from './phoneMask'
 export * from './cepMask'
 export * from './cnpjMask'
 export * from './cpfMask'
+export * from './cpfOrCnpj'
 export * from './utils'
+
+export type MasksName =
+  | 'phoneMask'
+  | 'cepMask'
+  | 'cnpjMask'
+  | 'cpfMask'
+  | 'cpfOrCnpj'
