cfcbf3dce72e728d8fc3e05cbd35c20837f86fc3
Gabriel Moraes <gabrielhm2001@gmail.com>
2021-09-27 01:01:13 -0300

    refactor phone mask

--- src/phone/index.ts
+++ src/phone/index.ts
@@ -1,16 +1,10 @@
 import { clear } from '../utils'
 
 export const phone = (phone: string) => {
-  const onlyPhoneNumbers = clear(phone)
-  const isPhoneLengthHigherThanTen = onlyPhoneNumbers.length > 10
-  const quantityOfCharsOnLeftSideOfDash = isPhoneLengthHigherThanTen ? 5 : 4
-  const regexToSeparateByDash = new RegExp(
-    `(\\d{${quantityOfCharsOnLeftSideOfDash}})(\\d)`
-  )
-
-  return onlyPhoneNumbers
+  return clear(phone)
     .replace(/(\d)/, '($1')
     .replace(/(\d{2})(\d)/, '$1) $2')
-    .replace(regexToSeparateByDash, '$1-$2')
-    .replace(/(-\d{4})\d+?$/, '$1')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(\d{4})\d+?$/, '$1')
 }
